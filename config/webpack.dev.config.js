a7799c5f3920f1318be11f47bd452edb20d3b50c
jielong <heyliudiao@gmail.com>
2018-12-04 16:30:23 +0800

    webpack init

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+
+    mode: 'development',
+    
+    entry: path.join(__dirname, '../App.js'),
+
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'bundle.js'
+    }
+}
