d420b3e5d0b0d92b428e43d47ad51fa65a40f301
jielong <heyliudiao@gmail.com>
2018-12-04 16:43:53 +0800

    babel config

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -3,11 +3,20 @@ const path = require('path');
 module.exports = {
 
     mode: 'development',
-    
+
     entry: path.join(__dirname, '../App.js'),
 
     output: {
         path: path.join(__dirname, '../dist'),
         filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                use: ['babel-loader?cacheDirectory=true']
+            }
+        ]
     }
 }
